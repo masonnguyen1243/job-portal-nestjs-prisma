104c1470d71f8b7b6edd23f4158f28e8bef3160f
masonnguyen1243 <meowjcisgod@gmail.com>
2025-07-28 21:23:49 +0700

    Create company

--- backend/src/company/company.controller.ts
+++ backend/src/company/company.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { CompanyService } from '@/company/company.service';
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard';
+import { RegisterCompanyDto } from '@/company/dto/company.dto';
 
 @Controller('company')
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('register')
+  async registerCompany(
+    @Body() registerCompanyDto: RegisterCompanyDto,
+    @Req() req: any,
+  ) {
+    const userId = req.user.id;
+    const result = await this.companyService.registerCompany(
+      userId,
+      registerCompanyDto,
+    );
+
+    return {
+      message: 'Company registered successfully',
+      succcess: true,
+      result,
+    };
+  }
 }
--- backend/src/company/company.service.ts
+++ backend/src/company/company.service.ts
@@ -1,4 +1,36 @@
+import { PrismaService } from '@/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { RegisterCompanyDto } from '@/company/dto/company.dto';
 
 @Injectable()
-export class CompanyService {}
+export class CompanyService {
+  constructor(private prisma: PrismaService) {}
+
+  async registerCompany(
+    userId: string,
+    registerCompanyDto: RegisterCompanyDto,
+  ) {
+    const { name, description, website, location, logo } = registerCompanyDto;
+
+    const existingCompany = await this.prisma.company.findUnique({
+      where: { name },
+    });
+
+    if (existingCompany) {
+      throw new Error('Company with this name already exists');
+    }
+
+    const company = await this.prisma.company.create({
+      data: {
+        name,
+        description,
+        website,
+        location,
+        logo,
+        userId,
+      },
+    });
+
+    return company;
+  }
+}
