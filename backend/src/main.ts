787d7efb7b723e895a3fbcd2d997e1ec2132374e
masonnguyen1243 <meowjcisgod@gmail.com>
2025-07-28 13:33:25 +0700

    Pisma integration and building schema

---
+++ backend/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '@/app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get('PORT');
+
+  //config cors
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    credentials: true,
+  });
+
+  await app.listen(port);
+}
+
+bootstrap();
